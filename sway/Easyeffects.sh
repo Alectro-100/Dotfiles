8ca203e8d12b4de83153bd971b00ded32561dad1
Raj Kushwaha <alectrojelly@gmail.com>
2025-06-14 18:16:20 +0530

    Edited my scripts

--- sway/Easyeffects.sh
+++ sway/Easyeffects.sh
@@ -1,15 +1,4 @@
-pkill easyeffects
+#!/bin/bash
 
-(swaymsg exec flatpak run com.github.wwmm.easyeffects)
-NAME=""
-while [[ "$NAME" != "Easy Effects" ]]; do
-    NAME="$(swaymsg --type get_tree -r | jq -r '.. | objects | select(.app_id == "com.github.wwmm.easyeffects") | .name')"
-    sleep 0.01
-done
+NAME= pkill easyeffects && swaymsg exec flatpak run com.github.wwmm.easyeffects & disown; while [[ "$NAME" != "Easy Effects" ]]; do NAME="$(swaymsg --type get_tree -r | jq -r '.. | objects | select(.app_id == "com.github.wwmm.easyeffects") | .name')"; sleep 0.01; done ; [[ $? -eq 0 ]] && { swaymsg '[app_id="com.github.wwmm.easyeffects"] move scratchpad' && notify-send -t 1000 "Successfully moved \"$NAME\" to scratchpad"; exit 0; } || { notify-send -t 1000 "Script didn't work"; }
 
-[[ $? -eq 0 ]] && swaymsg '[app_id="com.github.wwmm.easyeffects"] move scratchpad' && {
-    notify-send -t 1000 "Successfully moved \"$NAME\" to scratchpad"
-    exit 0
-}
-
-notify-send -t 1000 "Script didn't work"
